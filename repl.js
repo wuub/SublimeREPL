557f81705dada537d507beb12ee07fb3609f5333
Brandon Benvie <brandon@bbenvie.com>
2011-12-16 18:45:30 -0500

    Fix so it works with any path, bit of documentation

--- repl.js
+++ repl.js
@@ -1,3 +1,12 @@
-var repl = require('repl');
-repl.disableColors = true;
-repl.start('>', null, null, true);
+(function(){
+	var options = {
+		prompt:     null, //'> ',
+		source:     null, //process.stdin,
+		eval:       null, //require('vm').runInThisContext,
+		useGlobal:  true  //false
+	};
+
+	var repl = require('repl');
+	repl.disableColors = true;
+	repl.start(options.prompt, options.source, options.eval, options.useGlobal);
+})();
