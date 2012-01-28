7c87ce5fc4f9a9332766108172f652591a9b5497
Wojciech Bederski <bitbucket@wuub.net>
2012-01-28 15:34:03 +0100

    Example: Clojure sublime-commands and sublime-keymap.

--- run_existing_command.py
+++ run_existing_command.py
@@ -1,44 +1,47 @@
+import os
 import json
 import codecs
 import sublime
 import sublime_plugin
 
+SUBLIMEREPL_DIR = os.getcwdu()
+
 # yes, CommandCommmand :) 
 class RunExistingWindowCommandCommand(sublime_plugin.WindowCommand):
-	def run(self, cmd_id, filepath):
-		json_cmd = self._find_cmd(cmd_id, filepath)
+	def run(self, id, file):
+		path = os.path.join(SUBLIMEREPL_DIR, file)
+		json_cmd = self._find_cmd(id, path)
 		if not json_cmd:
 			return
-		cmd = json_cmd["command"]
 		args = json_cmd["args"] if "args" in json_cmd else None
-		self.window.run_command(cmd, args)
+		self.window.run_command(json_cmd["command"], args)
 	
-	def _find_cmd(self, cmd_id, filepath):
-		return self._find_cmd_in_file(cmd_id, filepath)
+	def _find_cmd(self, id, file):
+		return self._find_cmd_in_file(id, file)
 				
-	def _find_cmd_in_file(self, cmd_id, filepath):
+	def _find_cmd_in_file(self, id, file):
 
 		try:
-			with codecs.open(filepath, "r", "utf-8") as f:
+			with codecs.open(file, "r", "utf-8") as f:
 				lines = [line.split("//")[0] for line in f.readlines()]
 				data = json.loads("\n".join(lines))
-			return self._find_cmd_in_json(cmd_id, data)
+			return self._find_cmd_in_json(id, data)
 		except (IOError, ValueError), e:
 			print sublime.error_message(str(e))
 
-	def _find_cmd_in_json(self, cmd_id, json_object):
+	def _find_cmd_in_json(self, id, json_object):
 		if isinstance(json_object, list):
 			for elem in json_object:
-				cmd = self._find_cmd_in_json(cmd_id, elem)
+				cmd = self._find_cmd_in_json(id, elem)
 				if cmd:
 					return cmd
 		elif isinstance(json_object, dict):
-			if "id" in json_object and json_object["id"] == cmd_id:
+			if "id" in json_object and json_object["id"] == id:
 				return json_object
 			elif "children" in json_object:
-				return self._find_cmd_in_json(cmd_id, json_object["children"])
+				return self._find_cmd_in_json(id, json_object["children"])
 		return None
 		
 
 
-#sublime.active_window().run_command("run_existing_window_command", {"cmd_id": "hello"})
+#sublime.active_window().run_command("run_existing_window_command", {"id": "hello"})
