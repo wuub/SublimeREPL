036740d4053ac937c69bf2823c5f27f906300ff6
Wojciech Bederski <bitbucket@wuub.net>
2012-03-03 18:03:51 +0100

    Change a way console is hidden on windows to fix vPython ... and possibly other gui/GL toolkits windows from staying hidden

--- subprocess_repl.py
+++ subprocess_repl.py
@@ -6,37 +6,42 @@
 import subprocess
 import os
 import repl
+from sublime import load_settings
 
 class SubprocessRepl(repl.Repl):
     TYPE = "subprocess"
 
     def __init__(self, encoding, external_id=None, cmd_postfix="\n", suppress_echo=False, cmd=None, 
                  env=None, cwd=None, extend_env=None, soft_quit=""):
-        super(SubprocessRepl, self).__init__(encoding, external_id, cmd_postfix, suppress_echo)
+        super(SubprocessRepl, self).__init__(encoding, external_id, cmd_postfix, suppress_echo)        
+        settings = load_settings('SublimeREPL.sublime-settings')
+
         self._cmd = cmd
         self._soft_quit = soft_quit
+        env = self.env(env, extend_env, settings)
         self.popen = subprocess.Popen(
                         cmd, 
-                        startupinfo=self.startupinfo(),
+                        startupinfo=self.startupinfo(settings),
+                        creationflags=self.creationflags(settings),
                         bufsize=1, 
-                        cwd=self.cwd(cwd),
-                        env=self.env(env, extend_env),
+                        cwd=self.cwd(cwd, settings),
+                        env=env,
                         stderr=subprocess.STDOUT,
                         stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE)
 
-    def cwd(self, cwd):
+    def cwd(self, cwd, settings):
         import os.path
         if cwd and os.path.exists(cwd):
             return cwd
         return None
 
-    def env(self, env, extend_env):
+    def env(self, env, extend_env, settings):
         import os
-        from sublime import load_settings
+        
         updated_env = env if env else os.environ.copy()
 
-        default_extend_env = load_settings('SublimeREPL.sublime-settings').get("default_extend_env")
+        default_extend_env = settings.get("default_extend_env")
         if default_extend_env:
             updated_env.update(self.interpolate_extend_env(updated_env, default_extend_env))
 
@@ -60,12 +65,19 @@ class SubprocessRepl(repl.Repl):
             new_env[key] = str(val).format(**env)
         return new_env
 
-    def startupinfo(self):
+    def startupinfo(self, settings):
         startupinfo = None
         if os.name == 'nt':
             startupinfo = subprocess.STARTUPINFO()
-            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW 
+            startupinfo.wShowWindow |= 1 # SW_SHOWNORMAL
         return startupinfo
+
+    def creationflags(self, settings):
+        creationflags = 0
+        if os.name =="nt":
+            creationflags = 0x8000000 # CREATE_NO_WINDOW 
+        return creationflags
 
     def name(self):
         if self.external_id:
