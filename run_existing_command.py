4413608dae4c4a0aaf4d86baea69c40ccac1bbd3
Wojciech Bederski <github@wuub.net>
2012-05-28 18:50:49 +0200

    run_existing_command check User/SublimeREPL first. (add missing id's in Clojure+Python .sublime-menus

--- run_existing_command.py
+++ run_existing_command.py
@@ -5,12 +5,18 @@ import sublime
 import sublime_plugin
 
 SUBLIMEREPL_DIR = os.getcwdu()
+SUBLIMEREPL_USER_DIR = os.path.join(sublime.packages_path(), "User", "SublimeREPL")
 
 # yes, CommandCommmand :) 
 class RunExistingWindowCommandCommand(sublime_plugin.WindowCommand):
 	def run(self, id, file):
-		path = os.path.join(SUBLIMEREPL_DIR, file)
-		json_cmd = self._find_cmd(id, path)
+		"""Find and run existing command with id in specified file. 
+		SUBLIMEREPL_USER_DIR is consulted first, and then SUBLIMEREPL_DIR""" 
+		for prefix in (SUBLIMEREPL_USER_DIR, SUBLIMEREPL_DIR):
+			path = os.path.join(prefix, file)
+			json_cmd = self._find_cmd(id, path)
+			if json_cmd:
+				break
 		if not json_cmd:
 			return
 		args = json_cmd["args"] if "args" in json_cmd else None
@@ -20,14 +26,13 @@ class RunExistingWindowCommandCommand(sublime_plugin.WindowCommand):
 		return self._find_cmd_in_file(id, file)
 				
 	def _find_cmd_in_file(self, id, file):
-
 		try:
 			with codecs.open(file, "r", "utf-8") as f:
 				lines = [line.split("//")[0] for line in f.readlines()]
 				data = json.loads("\n".join(lines))
 			return self._find_cmd_in_json(id, data)
 		except (IOError, ValueError), e:
-			print sublime.error_message(str(e))
+			return None
 
 	def _find_cmd_in_json(self, id, json_object):
 		if isinstance(json_object, list):
