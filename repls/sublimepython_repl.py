81cc045d9b8354b3c5d0a6e3b3702df79dbda8ae
Wojciech Bederski <github@wuub.net>
2012-08-18 21:17:18 +0200

    make output instantly visible in sublime python repl

--- repls/sublimepython_repl.py
+++ repls/sublimepython_repl.py
@@ -4,11 +4,19 @@ import contextlib
 import repl
 from Queue import Queue
 import sys
+import threading
+
+class QueueOut(object):
+   def __init__(self, queue):
+        self.queue = queue
+   def write(self, data):
+        self.queue.put(data)
+
 
 @contextlib.contextmanager
-def redirect_stdio(sio):
+def redirect_stdio(queue):
     orig = (sys.stdout, sys.stderr)
-    sys.stdout = sys.stderr = sio
+    sys.stdout = sys.stderr = QueueOut(queue)
     yield
     (sys.stdout, sys.stderr) = orig
 
@@ -17,6 +25,7 @@ class InterceptingConsole(code.InteractiveConsole):
     PS2 = "... "
     def __init__(self, *args, **kwds):
         code.InteractiveConsole.__init__(self, *args, **kwds)
+        self.input = Queue()
         self.output = Queue()
         self.output.put(self.PS1)
 
@@ -24,14 +33,17 @@ class InterceptingConsole(code.InteractiveConsole):
         self.output.put(data)
 
     def push(self, line):
-        from StringIO import StringIO
-        s = StringIO()
-        with redirect_stdio(s):
+        with redirect_stdio(self.output):
             more = code.InteractiveConsole.push(self, line)
-        if s.len:
-            self.output.put(s.getvalue())
         self.output.put(self.PS2 if more else self.PS1)
         return more
+
+    def run(self):
+        while True:
+            line = self.input.get()
+            if line is None:
+                break
+            self.push(line)
 
 
 class SublimePythonRepl(repl.Repl):
@@ -40,6 +52,8 @@ class SublimePythonRepl(repl.Repl):
     def __init__(self, encoding):
         super(SublimePythonRepl, self).__init__(encoding, u"python", "\n", False)
         self._console = InterceptingConsole()
+        self._thread = threading.Thread(target=self._console.run)
+        self._thread.start()
 
     def name(self):
         return "sublime"
@@ -48,10 +62,10 @@ class SublimePythonRepl(repl.Repl):
         return True
 
     def write_bytes(self, bytes):
-        self._console.push(bytes)
-        
+        self._console.input.put(bytes)
+
     def read_bytes(self):
         return self._console.output.get()
 
     def kill(self):
-        pass
+        self._console.input.put(None)
