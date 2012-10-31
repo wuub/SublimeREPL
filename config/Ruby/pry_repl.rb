c676ec0b0b236674ae2fdc05786be0ad1e99e1f3
Wojciech Bederski <github@wuub.net>
2012-10-31 22:43:49 +0100

    move ruby to pry!

---
+++ config/Ruby/pry_repl.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+gem 'pry'
+require 'pry'
+
+class PryInput
+	def readline(prompt)
+		$stdout.print prompt
+		$stdin.readline
+	end
+end
+
+class PryOutput
+	def puts(data="")
+		$stdout.puts(data.gsub('`', "'"))
+	end
+end
+
+Pry.config.input = PryInput.new()
+Pry.config.output = PryOutput.new()
+Pry.config.color = false
+Pry.config.editor = ARGV[0]
+Pry.config.auto_indent = false
+Pry.config.correct_indent = false
+
+Pry.start self
