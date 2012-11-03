bf447d0a9262c4829225b5ad94c78b41ca30b2f6
Wojciech Bederski <github@wuub.net>
2012-11-03 16:23:18 +0100

    working IPython autocompletion: megadumb + not customizable but working

---
+++ completions.py
@@ -0,0 +1,29 @@
+import sublime
+import sublime_plugin
+
+import json
+import socket
+
+
+class SublimeREPLCompletions(sublime_plugin.EventListener):
+    def on_query_completions(self, view, prefix, locations):
+        if not view.settings().get("repl"):
+            return True
+        from sublimerepl import manager
+        rv = manager.repl_view(view)
+        whole_prefix = view.substr(sublime.Region(rv._output_end, locations[0]))
+        print view, prefix, locations, whole_prefix
+        flags = sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(5.0)
+        s.connect(("localhost", 9999))
+        try:
+            text_parts = whole_prefix.rsplit(' ', 1)
+            text = text_parts.pop()
+            s.sendall(json.dumps({"line": whole_prefix, "text": text, "cursor_pos": len(whole_prefix)}) + "\n")
+            res = json.loads(s.recv(65535))
+        finally:
+            s.close()
+
+        comp = [(x,x) for x in res[1]]
+        return (comp, flags)
--- config/Python/ipy_repl.py
+++ config/Python/ipy_repl.py
@@ -1,6 +1,11 @@
 import sys
+import json
+import socket
+import threading
+
 from IPython.frontend.terminal.embed import InteractiveShellEmbed
 from IPython.config.loader import Config
+
 
 editor = "subl -w"
 if len(sys.argv) > 1:
@@ -13,5 +18,21 @@ cfg.InteractiveShell.colors = "NoColor"
 cfg.InteractiveShell.editor = editor
 
 embedded_shell = InteractiveShellEmbed(config=cfg)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(("localhost", 9999))
+s.listen(1)
+
+def handle():
+    while True:
+        clisock = s.accept()[0].makefile()
+        req = json.loads(clisock.readline())
+        completions = embedded_shell.complete(**req)
+        clisock.write(json.dumps(completions) + "\n")
+        clisock.close()
+
+t = threading.Thread(target=handle)
+t.start()
 
 embedded_shell()
