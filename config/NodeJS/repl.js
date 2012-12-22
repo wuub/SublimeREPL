86fb83ae329b2ef7b2666ca8f4d491c5734246cb
Ivan <ivanca@gmail.com>
2012-12-22 00:14:12 -0500

    Fixing autocomplete for Node.JS
    Node now gets a string extracted from the begging until the caret; now it returns relevant auto-complete suggestions where it previously did not.

--- config/NodeJS/repl.js
+++ config/NodeJS/repl.js
@@ -21,6 +21,10 @@
         var index = strData.indexOf(":");
         var json = strData.slice(index+1, strData.length - 1)
         var inData = JSON.parse(json);
+        var wordIndex = inData.line.slice(inData.cursor_pos).search(/\b/);
+        if(wordIndex !== 0){
+            inData.line = inData.line.slice(0, inData.cursor_pos);
+        }
 
         var send = function (_, completions) {
             var comps = completions[0];
