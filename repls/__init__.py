351072b4ec118b414744ef94e8a0bd8cd964a9e9
Wojciech Bederski <github@wuub.net>
2013-04-21 10:38:22 +0200

    telnet repl works

--- repls/__init__.py
+++ repls/__init__.py
@@ -1,7 +1,7 @@
 from .repl import *
 from .subprocess_repl import *
 from .sublimehaskell_repl import *
-# from .execnet_repl import *  # disabled for now
-# from .telnet_repl import *
+from .telnet_repl import *
 from .sublimepython_repl import *
 # from .powershell_repl import *
+# from .execnet_repl import *  # disabled for now
