43327eabb9d80522f9013af5977fb0bd313b8dd8
Richard Minerich <richard.minerich@gmail.com>
2013-05-20 20:47:11 -0400

    Added some text massaging options to make the Haskell repl easier to use

--- repls/sublimehaskell_repl.py
+++ repls/sublimehaskell_repl.py
@@ -27,8 +27,41 @@ def ghci_append_package_db(cmd):
         cmd.extend(['-package-db', package_conf])
     return cmd
 
+def removeWhitespace(lines):
+    # remove lines that are completely whitespace
+    lines = [line for line in lines if not line.isspace()]
+    
+    # remove extra whitespace for more flexible block execution        
+    lineSpaces = [len(line) - len(line.lstrip()) for line in lines]
+    minSpaces = min(lineSpaces)
+    fixedLines = [line[minSpaces:] for line in lines]
+    return fixedLines
+
+def wrapMultilineSyntax(lines):
+    # append mutli-line syntax if more than one line
+    lineLen = len(lines)
+    fixedLines = lines + [os.linesep] if lineLen == 1 else [":{" + os.linesep] + lines + [os.linesep + ":}" + os.linesep]
+    return fixedLines
+
 class SublimeHaskellRepl(SubprocessRepl):
     TYPE = "sublime_haskell"
 
     def __init__(self, encoding, cmd=None, **kwds):
         super(SublimeHaskellRepl, self).__init__(encoding, cmd=ghci_append_package_db(cmd), **kwds)
+
+    def write(self, command):
+        setting_multiline = get_setting('format_multiline', True)
+        setting_trimwhitespace = get_setting('format_trim_whitespace', True)
+        
+        newCmd = ""
+        if command.isspace() or (not setting_multiline and not setting_trimwhitespace):
+                newCmd = command
+        else:
+                lines = command.splitlines(True)
+                if setting_trimwhitespace:
+                        lines = removeWhitespace(lines)
+                if setting_multiline:
+                        lines = wrapMultilineSyntax(lines)
+                newCmd = "".join(lines)
+
+	return super(HaskellRepl, self).write(newCmd)
