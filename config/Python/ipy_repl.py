6538c9a3053ed614c65bd475cd7736ab037dc493
S.-W. Liu <swliuproxy@users.noreply.github.com>
2015-09-08 21:48:30 +1000

    Fix IPython 4 auto-complete
    Thanks to brianhuey who made IPython 4 working in pull request: https://github.com/wuub/SublimeREPL/pull/440
    
    Here I further fix the auto complete function for IPython 4. The bug was caused by the ``kc.shell_channel.complete()`` function has been changed into ``kc.complete()`` with different parameters in IPython 4.

--- config/Python/ipy_repl.py
+++ config/Python/ipy_repl.py
@@ -89,8 +89,14 @@ def send_netstring(sock, msg):
 
 def complete(zmq_shell, req):
     kc = kernel_client(zmq_shell)
-    msg_id = kc.shell_channel.complete(**req)
-    msg = kc.shell_channel.get_msg(timeout=10)
+    # IPython 4+
+    if version > 3:
+        msg_id = kc.complete(req['line'], req['cursor_pos'])
+    # Ipython 1-3
+    else:
+        msg_id = kc.shell_channel.complete(**req)
+    
+    msg = kc.shell_channel.get_msg(timeout=0.5)
     if msg['parent_header']['msg_id'] == msg_id:
         return msg["content"]["matches"]
     return []
